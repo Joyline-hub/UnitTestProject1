70cd2e767e8911f5c18e8d3934a34928c90dc9bd
joyline.m <joyline.m@BG1NB683>
2020-08-03 19:03:37 +0530

    Add project files.

---
+++ UnitTestProject1/Base.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    public class Base
+    {
+        public IWebDriver Driver { get; set; }
+
+    }
+}
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class FirefoxTesting : Hooks
+    {
+        [Test]
+        public void FirefoxGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+
+        }
+    }
+
+    [TestFixture]
+    public class IETesting : Hooks
+    {
+        [Test]
+        public void IEGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.durgambabooking.com/");
+
+        }
+    }
+}
